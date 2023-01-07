8afe725fb0660c1fe9429bf8ed5bd208b622a6a5
InkoHX <me@inkohx.dev>
2023-01-08 00:00:31 +0900

    :tada: Initial commit.

---
+++ index.js
@@ -0,0 +1,44 @@
+import { Octokit } from 'octokit'
+import { config } from 'dotenv'
+
+config()
+
+const args = process.argv.slice(1)
+const dryRun = args.includes('--dry-run')
+const duration = new Date(Date.now() - 1000 * 60 * 60 * 24 * 365)
+const octkit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+const { data: user } = await octkit.rest.users.getAuthenticated()
+const repositories = octkit.paginate.iterator(octkit.rest.repos.listForUser, {
+  username: user.login,
+})
+
+if (dryRun)
+  console.warn('No archiving because the "--dry-run" option is enabled.')
+
+for await (const response of repositories) {
+  for (const repository of response.data) {
+    const updatedAt = new Date(repository.updated_at)
+
+    if (
+      repository.archived ||
+      repository.disabled ||
+      repository.fork ||
+      repository.private
+    )
+      continue
+    if (updatedAt > duration) continue
+
+    console.log(`${repository.full_name} is inactive.`)
+
+    if (!dryRun) {
+      await octkit.rest.repos.update({
+        owner: user.login,
+        repo: repository.name,
+        archived: true,
+      })
+      console.log(`${repository.full_name} is archived.`)
+    }
+  }
+}
