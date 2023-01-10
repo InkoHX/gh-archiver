dc8109b9b95a585269a4aed8f0281d2a480fb3c0
InkoHX <me@inkohx.dev>
2023-01-11 00:05:26 +0900

    :heavy_plus_sign: setup ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es2022: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+}
